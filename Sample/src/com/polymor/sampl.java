cf53154719107649b0be04b22415e935e89124da
ASAIKIRAN11 <asaikiran1108@gmail.com>
2024-08-27 10:04:01 +0530

    even number

---
+++ Sample/src/com/polymor/sampl.java
@@ -0,0 +1,14 @@
+package com.polymor;
+
+public class sampl {
+
+	public static void main(String[] args) {
+		for(int i=60;i<=80;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
